4a557af71833bf44546c127a34bfba2d8d39a24d
peesoda <sarahkwa@usc.edu>
2025-05-07 22:30:39 -0700

    Update cursor.js

--- js/cursor.js
+++ js/cursor.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const cursor = document.getElementById("custom-cursor");
   
     document.addEventListener("mousemove", (e) => {
-      cursor.style.left = `${e.clientX}px`;
-      cursor.style.top = `${e.clientY}px`;
+      cursor.style.left = `${e.clientX - 24}px`;
+      cursor.style.top = `${e.clientY - 24}px`;
     });
   
     const images = document.querySelectorAll("img");
@@ -13,12 +13,16 @@ document.addEventListener("DOMContentLoaded", () => {
         cursor.style.backgroundImage = 'url("../css/assets/lightning-bolt.png")';
         cursor.style.width = "64px";
         cursor.style.height = "64px";
+        cursor.style.left = `${parseInt(cursor.style.left) - 8}px`;
+        cursor.style.top = `${parseInt(cursor.style.top) - 8}px`;
       });
   
       img.addEventListener("mouseleave", () => {
         cursor.style.backgroundImage = 'url("../css/assets/star.png")';
         cursor.style.width = "48px";
         cursor.style.height = "48px";
+        cursor.style.left = `${parseInt(cursor.style.left) + 8}px`;
+        cursor.style.top = `${parseInt(cursor.style.top) + 8}px`;
       });
     });
   });
