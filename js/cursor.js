27cf35374722314c9e9700b1f6cc1306cc978119
peesoda <sarahkwa@usc.edu>
2025-05-07 21:54:12 -0700

    pngs

--- js/cursor.js
+++ js/cursor.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
     // Use absolute or corrected paths based on your repo location
-    const defaultCursor = 'url("..css/assets/star.svg") 16 16, auto';
-    const hoverCursor = 'url("..css/assets/lightning bolt.svg") 16 16, auto';
+    const defaultCursor = 'url("..css/assets/star.png") 16 16, auto';
+    const hoverCursor = 'url("..css/assets/lightning-bolt.png") 16 16, auto';
   
     // Apply default cursor to body
     document.body.style.cursor = defaultCursor;
