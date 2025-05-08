78d3d8edf4efe8787249946cb1fea5d36b98ece2
peesoda <sarahkwa@usc.edu>
2025-05-07 21:55:41 -0700

    Update cursor.js

--- js/cursor.js
+++ js/cursor.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    const defaultCursor = 'url("/css/assets/star.png") 16 16, auto';
-    const hoverCursor = 'url("/css/assets/lightning-bolt.png") 16 16, auto';
+    const defaultCursor = 'url("../css/assets/star.png") 16 16, auto';
+    const hoverCursor = 'url("../css/assets/lightning-bolt.png") 16 16, auto';
 
     // Set the default cursor on the entire page
     $('body').css('cursor', defaultCursor);
