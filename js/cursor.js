be5121608ef6d4460eeae9cb23ee6bd420251492
peesoda <sarahkwa@usc.edu>
2025-05-07 21:45:31 -0700

    Update cursor.js

--- js/cursor.js
+++ js/cursor.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
-    const defaultCursor = 'url("/zapadoodle/css/assets/star.svg") 16 16, auto';
-    const hoverCursor = 'url("/zapadoodle/css/assets/lightning bolt.svg") 16 16, auto';
+    const defaultCursor = 'url("../css/assets/star.svg") 16 16, auto';
+    const hoverCursor = 'url("../css/assets/lightning bolt.svg") 16 16, auto';
 
+
+    // Set the default cursor on the entire page
     $('body').css('cursor', defaultCursor);
 
+    // Change cursor when hovering over any <img>
     $('img').hover(
         function () {
             $(this).css('cursor', hoverCursor);
