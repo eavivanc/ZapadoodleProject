b5d75d12001917e78e6a2fefb7d47d23ebc7ce75
peesoda <sarahkwa@usc.edu>
2025-05-07 21:47:50 -0700

    Update cursor.js

--- js/cursor.js
+++ js/cursor.js
@@ -1,18 +1,20 @@
-$(document).ready(function () {
-    const defaultCursor = 'url("../css/assets/star.svg") 16 16, auto';
-    const hoverCursor = 'url("../css/assets/lightning bolt.svg") 16 16, auto';
-
-
-    // Set the default cursor on the entire page
-    $('body').css('cursor', defaultCursor);
-
-    // Change cursor when hovering over any <img>
-    $('img').hover(
-        function () {
-            $(this).css('cursor', hoverCursor);
-        },  
-        function () {
-            $(this).css('cursor', defaultCursor);
-        }
-    );
-});
+window.addEventListener("DOMContentLoaded", () => {
+    // Use absolute or corrected paths based on your repo location
+    const defaultCursor = 'url("..css/assets/star.svg") 16 16, auto';
+    const hoverCursor = 'url("..css/assets/lightning bolt.svg") 16 16, auto';
+  
+    // Apply default cursor to body
+    document.body.style.cursor = defaultCursor;
+  
+    // Get all images and attach event listeners
+    const images = document.querySelectorAll("img");
+  
+    images.forEach((img) => {
+      img.addEventListener("mouseenter", () => {
+        img.style.cursor = hoverCursor;
+      });
+      img.addEventListener("mouseleave", () => {
+        img.style.cursor = defaultCursor;
+      });
+    });
+  });
